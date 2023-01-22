6f6ce5cdb9f86b031ebd5b1a00498ecc3833adaf
TRICHA ABDELMOUGHIT <katsu.iope@gmail.com>
2023-01-22 02:34:10 +0100

    Buttons color activated

--- app/src/main/java/com/example/abdelmoughittricha/MainActivity.java
+++ app/src/main/java/com/example/abdelmoughittricha/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_reinitializer,btn_quitter;
+    Button btn_reinitializer,btn_quitter,color1,color2,color3;
     EditText number;
     TextView textview;
 
@@ -24,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
         btn_reinitializer=findViewById(R.id.r);
         btn_quitter=findViewById(R.id.q);
         number=findViewById(R.id.insert);
+        color1=findViewById(R.id.color1);
+        color2=findViewById(R.id.color2);
+        color3=findViewById(R.id.color3);
 
         btn_quitter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,5 +43,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        color1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setMyScreenColor(Color.DKGRAY);
+            }
+        });
+        color2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setMyScreenColor(Color.BLUE);
+            }
+        });
+        color3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setMyScreenColor(Color.GRAY);
+            }
+        });
+
+
+    }
+    public void setMyScreenColor(int color) {
+        View v = this.getWindow().getDecorView();
+        v.setBackgroundColor(color);
     }
 }
