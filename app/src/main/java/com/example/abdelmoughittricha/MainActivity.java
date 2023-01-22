b46039e2eb98077d86b86ab69634d3ef7dff094b
TRICHA ABDELMOUGHIT <katsu.iope@gmail.com>
2023-01-22 04:09:01 +0100

    Calculating operator fixed | Rénitialiser & TextView bug fixed

--- app/src/main/java/com/example/abdelmoughittricha/MainActivity.java
+++ app/src/main/java/com/example/abdelmoughittricha/MainActivity.java
@@ -2,17 +2,19 @@ package com.example.abdelmoughittricha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_reinitializer,btn_quitter;
+    Button btn_reinitializer,btn_quitter,btn_afficher;
     EditText number;
-    TextView textview;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +22,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btn_reinitializer=findViewById(R.id.r);
         btn_quitter=findViewById(R.id.q);
+        btn_afficher=findViewById(R.id.a);
         number=findViewById(R.id.insert);
+        tv=findViewById(R.id.tv);
+
+
+        btn_afficher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+
+
+                    int Number = Integer.parseInt(number.getText().toString());
+                    String s="";
+                    for (int i=1;i<=10;i++) {
+                        int r= Number;
+                        s+=String.format(" %d * %d= %d \n",Number,i,r);
+                        r=0;
+
+                    }
+                    tv.setText(s);
+
+
+                }catch (NumberFormatException e){
+                    btn_reinitializer.callOnClick();
+
+                }
+
+            }
+
+        });
+
+
 
         btn_quitter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,12 +62,18 @@ public class MainActivity extends AppCompatActivity {
                 System.exit(0);
             }
         });
-
         btn_reinitializer.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("DefaultLocale")
             @Override
-            public void onClick(View view) {
-                number.setText(null);
+            public void onClick(View v) {
+                number.setText("");
+                StringBuilder r = new StringBuilder();
+                for (int i = 0; i <= 10 ; i++) {
+                    r.append(String.format("? x %d = ? \n", i));
+                }
+                tv.setText(r.toString());
             }
         });
+
     }
 }
